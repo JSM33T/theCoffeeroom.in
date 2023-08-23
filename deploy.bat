9cbcd5bb6231a388f0a32b1ac7c9a113f15c9e36
jasmeet singh <jskainthofficial@gmail.com>
2023-08-23 15:41:29 +0530

    basic deploy batch

--- deploy.bat
+++ deploy.bat
@@ -1,7 +1,8 @@
 @echo off
+set "exclude_folder=wwwroot\assets\vendor"
 for /f %%x in ('powershell -command "Get-Date -Format 'yyyy-MM-dd'"') do set "current_date=%%x"
 set "commit_message=Automated update - %current_date%"
-git add .
+git rm --cached -r "%exclude_folder%"
 git commit -m "%commit_message%"
 git push origin main
 pause
