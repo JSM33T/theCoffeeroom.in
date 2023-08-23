7d6db295cb29f4dda9eb660b09bf166ede3bd9a8
jasmeet singh <jskainthofficial@gmail.com>
2023-08-23 15:46:02 +0530

    🏗️ plesk_deploy

--- deploy.bat
+++ deploy.bat
@@ -1,8 +1,13 @@
 @echo off
-set "exclude_folder=wwwroot\assets\vendor"
+set "exclude_folders=wwwroot\assets\vendor wwwroot\assets\img"
+
 for /f %%x in ('powershell -command "Get-Date -Format 'yyyy-MM-dd'"') do set "current_date=%%x"
 set "commit_message=Automated update - %current_date%"
-git rm --cached -r "%exclude_folder%"
+
+for %%f in (%exclude_folders%) do (
+    git rm --cached -r "%%f"
+)
+
 git commit -m "%commit_message%"
 git push origin main
 pause
