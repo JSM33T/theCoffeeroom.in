18782b02f7ba5626100a7bc43142cd9d042a7e7c
Jasmeet Singh <jskainthofficial@gmail.com>
2023-09-20 18:52:33 +0530

    :coffee: device sync

--- Models/Domain/TgBot.cs
+++ Models/Domain/TgBot.cs
@@ -1,11 +1,14 @@
-﻿namespace theCoffeeroom.Models.Domain
+﻿using Microsoft.AspNetCore.Components.Forms;
+
+namespace theCoffeeroom.Models.Domain
 {
     public class TgBot
     {
         public long UserId { get; set; }
         public string Message { get; set; }
         public string ImgAddress{ get; set; }
-
+        public string ImgAddress1 { get; set; }
+        public string ImgAddress2 { get; set; }
         public string Btn1Text { get; set; }
         public string Btn1Url { get; set; }
         public string Btn2Text { get; set; }
